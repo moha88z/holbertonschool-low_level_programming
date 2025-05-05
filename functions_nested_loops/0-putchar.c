205c45cc07d9598ac2e161f47a3fbe7f66933f98
moha88z <mohammed.alayda@gmail.com>
2025-05-05 00:21:38 +0000

    Add 0-putchar.c and main.h

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    _putchar('H');
+    _putchar('e');
+    _putchar('l');
+    _putchar('l');
+    _putchar('o');
+    _putchar('\n');
+
+    return (0);
+}
+
