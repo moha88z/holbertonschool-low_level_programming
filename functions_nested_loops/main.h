88f60fd0ded5f45b1663433b83140f15cae712dc
moha88z <mohammed.alayda88@gmail.com>
2025-05-07 08:47:26 +0000

    gg

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,7 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-int _putchar(char c);
-void print_alphabet(void);
-
-#endif
+int _islower(int c)
+{
+    if (c >= 'a' && c <= 'z')
+        return (1);
+    else
+        return (0);
+}
