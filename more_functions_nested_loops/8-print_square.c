a3b629130632d86412cf7bb3e49197db4a2ffd87
moha88z <mohammed.alayda88@gmail.com>
2025-05-10 08:37:53 +0000

    gg

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ * @size: size of the square
+ */
+void print_square(int size)
+{
+    if (size <= 0)
+    {
+        _putchar('\n');
+    }
+    else
+    {
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                _putchar('#');
+            }
+            _putchar('\n');
+        }
+    }
+}
