f2b29ba1af7c5b83de97937428193f7c4e4caa80
moha88z <mohammed.alayda88@gmail.com>
2025-05-10 07:57:53 +0000

    gg

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -2,9 +2,6 @@
 #define MAIN_H
 
 int _putchar(char c);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
+void print_most_numbers(void);
 
 #endif /* MAIN_H */
