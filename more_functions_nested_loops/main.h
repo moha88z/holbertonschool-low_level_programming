026a001f00d6cc67b1b4cbdb8deac202cf07c14d
moha88z <mohammed.alayda88@gmail.com>
2025-05-10 09:11:42 +0000

    gg

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,4 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#endif /* MAIN_H */
+int _putchar(char c);
+void print_triangle(int size);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+
+#endif
