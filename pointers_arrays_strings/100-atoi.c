c40a763ea9934b6d7e41209a31c500e9e376d2dd
moha88z <mohammed.alayda88@gmail.com>
2025-05-11 22:14:33 +0000

    gg

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,34 +8,36 @@
  */
 int _atoi(char *s)
 {
-    int i = 0;
-    int sign = 1;
-    int result = 0;
-    int started = 0;
+	int i = 0;
+	int sign = 1;
+	int result = 0;
+	int started = 0;
 
-    while (s[i])
-    {
-        if (s[i] == '-')
-            sign *= -1;
-        else if (s[i] >= '0' && s[i] <= '9')
-        {
-            started = 1;
+	while (s[i])
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			started = 1;
 
-            /* overflow handling */
-            if (result > (2147483647 - (s[i] - '0')) / 10)
-            {
-                if (sign == 1)
-                    return (2147483647);
-                else
-                    return (-2147483648);
-            }
+			/* overflow handling */
+			if (result > (2147483647 - (s[i] - '0')) / 10)
+			{
+				if (sign == 1)
+					return (2147483647);
+				else
+					return (-2147483648);
+			}
 
-            result = result * 10 + (s[i] - '0');
-        }
-        else if (started)
-            break;
-        i++;
-    }
+			result = result * 10 + (s[i] - '0');
+		}
+		else if (started)
+		{
+			break;
+		}
+		i++;
+	}
 
-    return (result * sign);
+	return (result * sign);
 }
