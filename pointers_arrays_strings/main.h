a4bada550e3f6983f168bfe4042503796a905e6e
moha88z <mohammed.alayda88@gmail.com>
2025-05-12 06:44:53 +0000

    gg

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,15 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-
-void _puts(char *str);
-void print_array(int *a, int n);
-void reverse_array(int *a, int n);
-int _strcmp(char *s1, char *s2);
-char *_strncpy(char *dest, char *src, int n);
-char *_strncat(char *dest, char *src, int n);
-char *_strcat(char *dest, char *src);
-int _putchar(char c);
+char *string_toupper(char *str);
 
 #endif /* MAIN_H */
