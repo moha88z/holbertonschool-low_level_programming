8a8a048b478c72c898843d251d657b56b3adbc15
moha88z <mohammed.alayda88@gmail.com>
2025-05-12 06:53:58 +0000

    gg

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,10 +2,13 @@
 #define MAIN_H
 
 int _putchar(char c);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int n);
-char *string_toupper(char *str);
-char *cap_string(char *str);
+char *leet(char *str);
 
 #endif
