696650a12c0177022859e823b822726841ca0573
moha88z <mohammed.alayda88@gmail.com>
2025-05-12 06:11:57 +0000

    gg

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,31 +1,33 @@
 #include "main.h"
 
 /**
- * _strncat - Concatenates two strings, using at most n bytes from src
- * @dest: The destination string
- * @src: The source string
- * @n: The maximum number of bytes to copy from src
+ * _strncat - Concatenates two strings, using at most n bytes from src.
+ * @dest: The destination string.
+ * @src: The source string.
+ * @n: The maximum number of bytes to be used from src.
  *
- * Return: Pointer to the resulting string dest
+ * Return: A pointer to the resulting string dest.
  */
 char *_strncat(char *dest, char *src, int n)
 {
-    int i = 0, j = 0;
+    int i = 0;
+    int dest_len = 0;
 
-    // Find the end of the destination string
-    while (dest[i] != '\0')
-        i++;
-
-    // Append n bytes from the source string
-    while (src[j] != '\0' && j < n)
+    /* Find the length of the destination string */
+    while (dest[dest_len] != '\0')
     {
-        dest[i] = src[j];
-        i++;
-        j++;
+        dest_len++;
     }
 
-    // Null-terminate the resulting string
-    dest[i] = '\0';
+    /* Append at most n bytes from src to dest */
+    while (i < n && src[i] != '\0')
+    {
+        dest[dest_len + i] = src[i];
+        i++;
+    }
+
+    /* Add the null terminator */
+    dest[dest_len + i] = '\0';
 
     return dest;
 }
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stddef.h>  // for size_t
+#include <stddef.h>  /* for size_t */
 
 char *_strncat(char *dest, char *src, int n);
 int _putchar(char c);
