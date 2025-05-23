cd7426dbfc7e78f3bdbcd7a765cbb1fb43dd7883
moha88z <mohammed.alayda88@gmail.com>
2025-05-23 14:12:01 +0000

    gg

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure that defines a dog
+ * @name: name of the dog (string)
+ * @age: age of the dog (float)
+ * @owner: name of the dog’s owner (string)
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
