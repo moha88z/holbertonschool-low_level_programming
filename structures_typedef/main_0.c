9b5b46c6dafdfeb5ce8e9900b5c9ba74ecef2d7c
moha88z <mohammed.alayda88@gmail.com>
2025-05-25 15:47:20 +0000

    gg

--- structures_typedef/main_0.c
+++ structures_typedef/main_0.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include "dog.h"
 
+/**
+ * main - check the code for new_dog
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-    struct dog my_dog;
+	dog_t *my_dog;
 
-    my_dog.name = "Ghost";
-    my_dog.age = 4.75;
-    my_dog.owner = "Jon Snow";
+	my_dog = new_dog("Ghost", 4.75, "Jon Snow");
+	if (my_dog == NULL)
+		return (1);
 
-    print_dog(&my_dog);
-
-    return (0);
+	printf("My name is %s, and I am %.2f :) - Woof!\n", my_dog->name, my_dog->age);
+	return (0);
 }
