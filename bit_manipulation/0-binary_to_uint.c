ba7d4cba5591378b342408c160e9ba878bf561e2
moha88z <mohammed.alayda88@gmail.com>
2025-06-18 06:59:28 +0000

    gg

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stddef.h>
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (*b)
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+		result = result * 2 + (*b - '0');
+		b++;
+	}
+	return (result);
+}
