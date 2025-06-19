29fda129dbb777121741655724ccb8d2ae8dccea
moha88z <mohammed.alayda88@gmail.com>
2025-06-19 12:13:51 +0000

    gg

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -11,7 +11,7 @@
  * @argc: Number of arguments
  * @argv: Argument vector
  *
- * Return: 0 on success, exit with code on failure
+ * Return: 0 on success, exits with specific codes on failure
  */
 int main(int argc, char *argv[])
 {
@@ -39,24 +39,26 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	while ((r = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	r = read(fd_from, buffer, BUFFER_SIZE);
+	while (r > 0)
 	{
-		if (r == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			close(fd_from);
-			close(fd_to);
-			exit(98);
-		}
-
 		w = write(fd_to, buffer, r);
-		if (w == -1 || w != r)
+		if (w == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(fd_from);
 			close(fd_to);
 			exit(99);
 		}
+		r = read(fd_from, buffer, BUFFER_SIZE);
+	}
+
+	if (r == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close(fd_from);
+		close(fd_to);
+		exit(98);
 	}
 
 	if (close(fd_from) == -1)
