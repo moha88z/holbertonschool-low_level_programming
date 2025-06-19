8b76d40e4428590120dc7314e38fad5727c83368
moha88z <mohammed.alayda88@gmail.com>
2025-06-19 11:17:32 +0000

    gg

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -17,9 +17,11 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	/* O_CREAT: create file if it doesn't exist
-	   O_WRONLY: open for writing only
-	   O_TRUNC: if file exists, empty it (truncate it) */
+	/*
+     * O_CREAT: create file if it doesn't exist
+     * O_WRONLY: open for writing only
+     * O_TRUNC: if file exists, empty it (truncate it)
+     */
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
