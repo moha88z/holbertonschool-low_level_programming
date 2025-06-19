85edc07b912d819c5ccd4cb1de0fca25b2b7e53b
moha88z <mohammed.alayda88@gmail.com>
2025-06-19 11:46:36 +0000

    gg

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -24,7 +24,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		while (text_content[len])
 			len++;
-		
+
 		w = write(fd, text_content, len);
 		if (w == -1 || w != len)
 		{
